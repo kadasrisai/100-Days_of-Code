3ccb72b4f9950a64830bcbf059ca8e6a5ca9cb5a
srisaikada <srisaikada123@gmail.com>
2024-05-24 18:42:36 +0530

    Day-43 problems on backrtracking Level:Medium

---
+++ Day-43/Combination_Sum_II.py
@@ -0,0 +1,31 @@
+'''Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+
+Each number in candidates may only be used once in the combination.
+
+Note: The solution set must not contain duplicate combinations.'''
+
+class Solution(object):
+    def comb(self, i, sub, ans, candidates, t):
+        if t == 0:
+            ans.append(list(sub))
+            return
+        if i == len(candidates) or t < candidates[i]:
+            return
+        for ind in range(i, len(candidates)):
+            if ind > i and candidates[ind - 1] == candidates[ind]:
+                continue
+            sub.append(candidates[ind])
+            self.comb(ind + 1, sub, ans, candidates, t - candidates[ind])
+            sub.pop()
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        ans = []
+        sub = []
+        candidates.sort()
+        self.comb(0, sub, ans, candidates, target)
+        return ans
+        
---
+++ Day-43/permutations.py
@@ -0,0 +1,32 @@
+'''Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]'''
+
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        if len(nums) == 1:
+            return [nums[:]]
+        
+        res = []
+
+        for _ in range(len(nums)):
+            n = nums.pop(0)
+            perms = self.permute(nums)
+
+            for p in perms:
+                p.append(n)
+            
+            res.extend(perms)
+            nums.append(n)
+        
+        return res
+        
