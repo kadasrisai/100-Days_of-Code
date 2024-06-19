e2898c8c2a5d094107fc80275510d8c9de64039c
srisaikada <srisaikada123@gmail.com>
2024-06-19 23:29:42 +0530

    Pronbelms on heap Level:Easy

---
+++ Day-63/Merge_K_sorted_Array.py
@@ -0,0 +1,46 @@
+'''Given K sorted arrays arranged in the form of a matrix of size K*K. The task is to merge them into one sorted array.
+Example 1:
+
+Input:
+K = 3
+arr[][] = {{1,2,3},{4,5,6},{7,8,9}}
+Output: 1 2 3 4 5 6 7 8 9
+Explanation:Above test case has 3 sorted
+arrays of size 3, 3, 3
+arr[][] = [[1, 2, 3],[4, 5, 6], 
+[7, 8, 9]]
+The merged list will be 
+[1, 2, 3, 4, 5, 6, 7, 8, 9].
+Example 2:
+
+Input:
+K = 4
+arr[][]={{1,2,3,4},{2,2,3,4},
+         {5,5,6,6},{7,8,9,9}}
+Output:
+1 2 2 2 3 3 4 4 5 5 6 6 7 8 9 9 
+Explanation: Above test case has 4 sorted
+arrays of size 4, 4, 4, 4
+arr[][] = [[1, 2, 2, 2], [3, 3, 4, 4],
+[5, 5, 6, 6], [7, 8, 9, 9 ]]
+The merged list will be 
+[1, 2, 2, 2, 3, 3, 4, 4, 5, 5, 
+6, 6, 7, 8, 9, 9].'''
+
+class Solution:
+    #Function to merge k sorted arrays.
+    def mergeKArrays(self, arr, K):
+        # code here
+        # return merged list
+        D = []   #assign the empty
+        
+        #loop through for K
+        for i in range(K):
+            for j in range(K):
+                D.append(arr[i][j])
+
+
+        #sort the D
+        D.sort()
+        
+        return D   
