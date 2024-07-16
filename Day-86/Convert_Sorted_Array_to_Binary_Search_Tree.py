432f8a9393a2aa84eec3209420ff34b6ff4eec5b
srisaikada <srisaikada123@gmail.com>
2024-07-16 21:37:23 +0530

    Problem on BST Level:Easy

---
+++ Day-86/Convert_Sorted_Array_to_Binary_Search_Tree.py
@@ -0,0 +1,32 @@
+''' Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+ 
+
+Example 1:
+
+
+Input: nums = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+Example 2:
+
+
+Input: nums = [1,3]
+Output: [3,1]
+Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+  '''
+
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        def helper(start, end):
+            if start > end:
+                return None
+            mid = (start + end) // 2
+            root = TreeNode(nums[mid])
+            root.left = helper(start, mid - 1)
+            root.right = helper(mid + 1, end)
+            return root
+
+        return helper(0, len(nums) - 1)
+        
